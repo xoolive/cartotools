08f05ec2c8bee2e636ff74ce590624f27b53ae84
Xavier Olive <xavier@xoolive.org>
2017-11-27 22:42:24 +0100

    Autoload new tile servers from files included

--- cartotools/img_tiles/__init__.py
+++ cartotools/img_tiles/__init__.py
@@ -1,8 +1,29 @@
 from cartopy.io.img_tiles import *
 
-from .cached import Cache
-from .oaci import AIP_Drones, DFS_OACI
+import os.path
+
+from pathlib import Path
+from importlib import import_module
+
+for f_py in Path(os.path.dirname(__file__)).glob('[a-z]*.py'):
+    mdl = import_module(".{}".format(f_py.with_suffix("").name), __name__)
+    if "__all__" in mdl.__dict__:
+        # is there an __all__?  if so respect it
+        names = mdl.__dict__["__all__"]
+    else:
+        # otherwise we import all names that don't begin with _
+        names = [x for x in mdl.__dict__ if not x.startswith("_")]
+
+    # now drag them in
+    globals().update({k: getattr(mdl, k) for k in names})
+
 
 class GoogleTiles(Cache, GoogleTiles):
     pass
 
+
+class OSM(Cache, OSM):
+    pass
+
+class StamenTerrain(Cache, StamenTerrain):
+    pass
--- cartotools/img_tiles/oaci.py
+++ cartotools/img_tiles/oaci.py
@@ -1,13 +1,14 @@
 from . import GoogleTiles
+from .cached import Cache
 
-class DFS_OACI(GoogleTiles):
+class DFS_OACI(Cache, GoogleTiles):
 
     def _image_url(self, tile):
         x, y, z = tile
         return "https://secais.dfs.de/static-maps/ICAO500-2015-EUR-Reprojected_07/tiles/%s/%s/%s.png" % (z, x, y)
 
 
-class AIP_Drones(GoogleTiles):
+class AIP_Drones(Cache, GoogleTiles):
 
     def _image_url(self, tile):
         x, y, z = tile
