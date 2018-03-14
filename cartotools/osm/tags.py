12de4bf37874e37faa07fde3dd42c4e3fa156dce
Xavier Olive <xavier@xoolive.org>
2018-03-14 15:52:31 +0100

    tags

---
+++ cartotools/osm/tags.py
@@ -0,0 +1,13 @@
+windmills = {'infrastructure': 'node["power"]',
+             'filters': '["generator:source"~"wind"]'}
+
+airports = {'infrastructure': 'way["aeroway"]',
+            'filters': ''}
+
+churches = {'infrastructure': 'way["amenity"]',
+            'filters':'["amenity"~"place_of_worship"]'}
+
+parks = {'infrastructure': 'way["leisure"]',
+         'filters':'["leisure"~"park"]'}
+
+
