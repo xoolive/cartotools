1c8c95e0d00988b0a482b0359f8423f05caf26bf
Xavier Olive <git@xoolive.org>
2018-07-20 13:41:27 +0200

    version number

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name="cartotools",
-      version="0.1",
+      version="1.0",
       description="Making cartopy suit my needs",
       license="MIT",
       packages=['cartotools', 'cartotools.crs', 'cartotools.img_tiles',
